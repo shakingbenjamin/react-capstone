b702ff561b3ecd46ad5fcb651d7ce61b84a93f07
Benjamin <mr.shaking.benjamin@gmail.com>
2024-10-20 18:52:52 +0100

    Category List and Item

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+﻿import './category-item.styles.scss'
+
+const CategoryItem = ({category}) => {
+    const {title, subtitle, imageUrl} = category
+    return (
+        <div className="category-container">
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }}/>
+
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <div className="p">{subtitle}</div>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,49 @@
+﻿import './category-list.styles.scss'
+import CategoryItem from "../category-item/category-item.component";
+
+const CategoryList = () => {
+
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats',
+            subtitle: 'Shop Now',
+            imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+            subtitle: 'Shop Now',
+            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+            subtitle: 'Shop Now',
+            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+        },
+        {
+            id: 4,
+            title: 'Womens',
+            subtitle: 'Shop Now',
+            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+        },
+        {
+            id: 5,
+            title: 'Mens',
+            subtitle: 'Shop Now',
+            imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+        }
+    ]
+    
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category}/>
+            ))
+            }
+        </div>
+    )
+}
+
+export default CategoryList
